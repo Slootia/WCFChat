e93de8ddfcbf309160d2e3cf77d00f1502d8532f
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2019-08-09 11:58:15 +0500

    Добавил хост

---
+++ WCFHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WCFHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(WCFChat.ServiceChat)))
+            {
+                host.Open();
+                Console.WriteLine("Host started!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
