1fa09f43dbdf094c26a2a89f7d5728804c09c8c9
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2019-08-09 11:40:38 +0500

    Добавил сервис

---
+++ WCFChat/ServerUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFChat
+{
+    class ServerUser
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public OperationContext OperationContext { get; set; }
+    }
+}
---
+++ WCFChat/ServiceChat.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+
+namespace WCFChat
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ServiceChat : IServiceChat
+    {
+        List<ServerUser> users = new List<ServerUser>();
+        int nextId = 1;
+        public int Connect(string name)
+        {
+            ServerUser user = new ServerUser()
+            {
+                ID = nextId,
+                Name = name,
+                OperationContext = OperationContext.Current
+            };
+            nextId++;
+            SendMessage(user.Name + " подключился к чату!", 0);
+
+            users.Add(user);
+            return user.ID;
+        }
+
+        public void Disconnect(int id)
+        {
+            var user = users.FirstOrDefault(u => u.ID == id);
+            if (user != null)
+            {
+                users.Remove(user);
+                SendMessage(user.Name + "покинул чат!", 0);
+            }
+        }
+
+        public void SendMessage(string msg, int id)
+        {
+            foreach (var user in users)
+            {
+                string answer = DateTime.Now.ToShortTimeString();
+                var sender = users.FirstOrDefault(u => u.ID == id);
+                if (sender != null)
+                {
+                    answer += ": " + user.Name + " ";
+                }
+                answer += msg;
+
+                user.OperationContext.GetCallbackChannel<IServerChatCallback>().MessageCallback(answer);
+            }
+        }
+    }
+}
